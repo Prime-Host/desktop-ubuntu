fa8216d847f0f54527e32db4bbd93de3b40f6976
Prime Host <gregorius.stahlbart@gmail.com>
2019-03-19 15:22:55 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,71 +1,18 @@
-#Login as root before start
-apt-get update
-apt-get upgrade -y
+# General
+sudo apt-get update
+sudo apt-get dist-upgrade -y
+sudo apt install -y gnome-tweak-tool htop zsh curl wget git
 
-#KeePassX
-apt-get install keepassx -y
-
-#Chrome
-wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add - 
-sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list'
-apt-get update 
-apt-get install google-chrome-stable -y
-
-#Spotify
-echo deb http://repository.spotify.com stable non-free | sudo tee /etc/apt/sources.list.d/spotify.list 
-apt-key adv --recv-keys --keyserver keyserver.ubuntu.com BBEBDCB318AD50EC6865090613B00F1FD2C19886
-apt-get update
-apt-get install spotify-client -y
-
-#Git
-apt-get install git -y
-
-#Docker
-apt-get update
-apt-get upgrade -y
-apt-get install vim htop iftop git curl -y
-
-apt-get update
-apt-get install apt-transport-https ca-certificates
-apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
-echo "deb https://apt.dockerproject.org/repo ubuntu-xenial main" >> /etc/apt/sources.list.d/docker.list
-apt-get update
-apt-get purge lxc-docker
-apt-cache policy docker-engine
-apt-get update
-apt-get install linux-image-extra-$(uname -r) -y
-apt-get update
-apt-get install docker-engine -y
-
-curl -L https://github.com/docker/compose/releases/download/1.8.0/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
-chmod +x /usr/local/bin/docker-compose
-
-echo "export HISTFILESIZE=" >> /root/.bashrc
-echo "export HISTSIZE=" >> /root/.bashrc
-
-#Gnome Tweak Tool
-apt-get install gnome-tweak-tool -y
-#Install these Extensions: Activities configurator, Das to dock, Mmod Panel, Pixel saver, User Themes
-
-#oh-my-zsh
-apt-get install zsh -y
+# install oh-my-zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+cp ~/.oh-my-zsh/themes/bira.zsh-theme ~/.oh-my-zsh/themes/prime-host.zsh-theme \
+ && sed -i 's/%m%/%M%/g' ~/.oh-my-zsh/themes/prime-host.zsh-theme \
+ && sed -i s:~/.oh-my-zsh:\$HOME/.oh-my-zsh:g ~/.zshrc \
+ && sed -i 's/robbyrussell/prime-host/g' ~/.zshrc \
+ && echo "DISABLE_UPDATE_PROMPT=true" >> ~/.zshrc
 
-#Atom
-add-apt-repository ppa:webupd8team/atom
-apt-get update
-apt-get install atom
-
-#Conky Manager
-apt-add-repository -y ppa:teejee2008/ppa
-apt-get update
-apt-get install conky-manager -y
-
-#Enrytion GUI
-apt-get install seahorse 
-apt-get install seahorse-nautilus 
-
-#Upgrade all
-apt-get update
-apt-get upgrade
-apt-get dist-upgrade
+# install chrome
+wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
+echo 'deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main' | sudo tee /etc/apt/sources.list.d/google-chrome.list
+sudo apt-get update 
+sudo apt-get install google-chrome-stable
